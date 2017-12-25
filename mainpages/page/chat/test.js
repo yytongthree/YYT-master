357d7a4910c95c28368d56453d0958b1facb5322
wenny <wennyluo@qq.com>
2017-12-26 02:43:17 +0800

    add chat history
    增加聊天历史记录

--- mainpages/page/chat/test.js
+++ mainpages/page/chat/test.js
@@ -26,7 +26,7 @@ layui.config({
     });
 
     //加载数据
-	var chatsData = '';
+	var chatData = '';
 	//setInterval(function(){
 		$.get("chatroom.php",function(data){
 			chatData = data;
@@ -65,7 +65,7 @@ layui.config({
     })
 	
 	//查看历史记录
-    $("body").on("click",".history ,.msgHtml .user_info h2,.msgHtml .msg_info>img",function(){
+    $("body").on("click",".history",function(){
         var index = layui.layer.open({
             title : "聊天室历史记录",
             type : 2,
@@ -77,30 +77,65 @@ layui.config({
                 var body = layui.layer.getChildFrame('body', index);
                 //加载回复信息
                 $.get("chatroom.php",function(data){
-		/*			alert(data);
-					data = JSON.parse(data)
-					alert(data);
+				/*	data = JSON.parse(data)
                     var msgReplyHtml = '';
-					alert(data.length);
-                    for(var i=0; i<data.length; i++){
+					for(var i=0; i<data.length; i++){
 						msgReplyHtml += '<tr>';
 						msgReplyHtml += '  <td class="msg_info">';
 						msgReplyHtml += '    <img src="../../images/face.jpg" width="50" height="50">';
 						msgReplyHtml += '    <div class="user_info">';
-						msgReplyHtml += '        <h2>'+currData[i].name+'</h2>';
-						msgReplyHtml += '        <p>'+currData[i].content+'</p>';
+						msgReplyHtml += '        <h2>'+data[i].name+'</h2>';
+						msgReplyHtml += '        <p>'+data[i].content+'</p>';
 						msgReplyHtml += '    </div>';
 						msgReplyHtml += '  </td>';
-						msgReplyHtml += '  <td class="msg_time">'+currData[i].saytime+'</td>';
+						msgReplyHtml += '  <td class="msg_time">'+data[i].saytime+'</td>';
 						msgReplyHtml += '</tr>';
-                    }
-					alert(msgReplyHtml);
+                  	}
                     body.find(".msgReplyHtml").html(msgReplyHtml);*/
-					chatData = data;
-					chatData = JSON.parse(chatData);
-					usersList();
+					historyData = data;
+					historyData = JSON.parse(historyData);
+					historyList();
                 })
+			/*	function historyList(){
+					//渲染数据
+					function renderDate1(data,curr){
+						var historyHtml = '';
+						currData = historyData.concat().splice(curr*nums-nums, nums);
+						if(currData.length != 0){
+							for(var i=0; i<currData.length; i++){
+								historyHtml += '<tr>';
+								historyHtml += '  <td class="msg_info">';
+								historyHtml += '    <img src="../../images/face.jpg" width="50" height="50">';
+								historyHtml += '    <div class="user_info">';
+								historyHtml += '        <h2>'+currData[i].name+'</h2>';
+								historyHtml += '        <p>'+currData[i].content+'</p>';
+								historyHtml += '    </div>';
+								historyHtml += '  </td>';
+								historyHtml += '  <td class="msg_time">'+currData[i].saytime+'</td>';
+								historyHtml += '</tr>';
+							}
+						}
+						return historyHtml;
+					}
+					//分页
+					var nums = 3; //每页出现的数据量
+					laypage({
+						cont : "page_history",
+						pages : Math.ceil(historyData.length/nums),
+						curr: location.hash.replace('#!fenye=', ''), //获取hash值为fenye的当前页
+    					hash: 'fenye',
+						jump : function(obj,first){
+							if(!first){
+								layer.msg('第'+obj.curr+'页');
+							}
+							$(".historyHtml").html(renderDate1(historyData,obj.curr));
+							$('.users_list thead input[type="checkbox"]').prop("checked",false);
+							form.render();
+						}//Math.ceil()执行向上舍入，即它总是将数值向上舍入为最接近的整数；
+					});
+				}*/
             }
+			
         })
         //改变窗口大小时，重置弹窗的高度，防止超出可视区域（如F12调出debug的操作）
         $(window).resize(function(){
@@ -109,7 +144,7 @@ layui.config({
         layui.layer.full(index);
     })
 	
-	function usersList(){
+	function usersList(nums){
 		//渲染数据
 		function renderDate(data,curr){
 			var msgHtml = '';
@@ -131,78 +166,61 @@ layui.config({
 			return msgHtml;
 		}
 		//分页
-	//	var nums = 3; //每页出现的数据量
-	/*	laypage({
-			cont : "page",
-			pages : Math.ceil(chatData.length/nums),
-			curr: location.hash.replace('#!fenye=', ''), //获取hash值为fenye的当前页
-    		hash: 'fenye',
-			jump : function(obj,first){
-				if(!first){
-					layer.msg('第'+obj.curr+'页');
+		var nums = 3; //每页出现的数据量
+			laypage({
+				cont : "page",
+				pages : Math.ceil(chatData.length/nums),
+				curr: location.hash.replace('#!fenye=', ''), //获取hash值为fenye的当前页
+    			hash: 'fenye',
+				jump : function(obj,first){
+					if(!first){
+						layer.msg('第'+obj.curr+'页');
+					}
+					$(".msgHtml").html(renderDate(chatData,obj.curr));
+					$('.users_list thead input[type="checkbox"]').prop("checked",false);
+					form.render();
+				}//Math.ceil()执行向上舍入，即它总是将数值向上舍入为最接近的整数；
+			});
+			function historyList(){
+					//渲染数据
+					function renderDate1(data,curr){
+						var historyHtml = '';
+						currData1 = historyData.concat().splice(curr*num-num, num);
+						if(currData1.length != 0){
+							for(var i=0; i<currData1.length; i++){
+								historyHtml += '<tr>';
+								historyHtml += '  <td class="msg_info">';
+								historyHtml += '    <img src="../../images/face.jpg" width="50" height="50">';
+								historyHtml += '    <div class="user_info">';
+								historyHtml += '        <h2>'+currData1[i].name+'</h2>';
+								historyHtml += '        <p>'+currData1[i].content+'</p>';
+								historyHtml += '    </div>';
+								historyHtml += '  </td>';
+								historyHtml += '  <td class="msg_time">'+currData1[i].saytime+'</td>';
+								historyHtml += '</tr>';
+							}
+						}
+						return historyHtml;
+					}
+					//分页
+					var num = 3; //每页出现的数据量
+					laypage({
+						cont : "page_history",
+						pages : Math.ceil(historyData.length/num),
+						curr: location.hash.replace('#!fenye=', ''), //获取hash值为fenye的当前页
+    					hash: 'fenye',
+						jump : function(obj,first){
+							if(!first){
+								layer.msg('第'+obj.curr+'页');
+							}
+							body.find(".historyHtml").html(renderDate1(historyData,obj.curr));
+						//	$(".historyHtml").html(renderDate1(historyData,obj.curr));
+						//	$('.users_list thead input[type="checkbox"]').prop("checked",false);
+							form.render();
+						}//Math.ceil()执行向上舍入，即它总是将数值向上舍入为最接近的整数；
+					});
 				}
-				$(".msgHtml").html(renderDate(chatData,obj.curr));
-				$('.users_list thead input[type="checkbox"]').prop("checked",false);
-				form.render();
-			}//Math.ceil()执行向上舍入，即它总是将数值向上舍入为最接近的整数；
-		});*/
-		var nums=10;
-		laypage({
-			cont : "page",
-			pages : Math.ceil(chatData.length/num),
-			curr: location.hash.replace('#!fenye=', ''), //获取hash值为fenye的当前页
-    		hash: 'fenye',
-			jump : function(obj,first){
-				if(!first){
-					layer.msg('第'+obj.curr+'页');
-				}
-				$(".msgHtml").html(renderDate(chatData,obj.curr));
-			//	$('.users_list thead input[type="checkbox"]').prop("checked",false);
-				form.render();
-			}//Math.ceil()执行向上舍入，即它总是将数值向上舍入为最接近的整数；
-		});
+	//	}
 	}
-			
-/*	function chat_history(){
-		//渲染数据
-		function renderDate1(data,curr){
-			alert(data)
-			var historyHtml = '';
-			currData = historyData.concat().splice(curr*nums-nums, nums);
-			if(currData.length != 0){
-				for(var i=0; i<currData.length; i++){
-					historyHtml += '<tr>';
-					historyHtml += '  <td class="msg_info">';
-					historyHtml += '    <img src="../../images/face.jpg" width="50" height="50">';
-					historyHtml += '    <div class="user_info">';
-					historyHtml += '        <h2>'+currData[i].name+'</h2>';
-					historyHtml += '        <p>'+currData[i].content+'</p>';
-					historyHtml += '    </div>';
-					historyHtml += '  </td>';
-					historyHtml += '  <td class="msg_time">'+currData[i].saytime+'</td>';
-					historyHtml += '</tr>';
-				}
-			}
-			return historyHtml;
-		}
-		//分页
-		var nums = 10; //每页出现的数据量
-		alert(historyData);
-		alert(historyData.length);
-		laypage({
-			cont : "page_history",
-			pages : Math.ceil(historyData.length/nums),
-			curr: location.hash.replace('#!fenye=', ''), //获取hash值为fenye的当前页
-    		hash: 'fenye',
-			jump : function(obj,first){
-				if(!first){
-					layer.msg('第'+obj.curr+'页');
-				}
-				$(".historyHtml").html(renderDate1(historyData,obj.curr));
-				$('.users_list thead input[type="checkbox"]').prop("checked",false);
-				form.render();
-			}//Math.ceil()执行向上舍入，即它总是将数值向上舍入为最接近的整数；
-		})
-	}*/
 })
 
