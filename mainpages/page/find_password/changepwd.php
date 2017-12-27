d69d595be4cfc0cd13e99e4ae055624d127bc642
wenny <wennyluo@qq.com>
2017-12-27 17:18:22 +0800

    notice
    锁屏提示

--- mainpages/page/find_password/changepwd.php
+++ mainpages/page/find_password/changepwd.php
@@ -31,7 +31,7 @@
 	{
 		die('Could not connect: ' . mysqli_error());
 	}
-	
+	echo $_COOKIE['username'];
 	if($_COOKIE['username']){
 		$username=$_COOKIE['username'];
 		$sql = "UPDATE register_info SET password='$newpwd' WHERE name='{$username}'";
@@ -55,7 +55,6 @@
 		if($_COOKIE['email'] && $_COOKIE['token']){
 			$email=$_COOKIE['email'];
 			$token=$_COOKIE['token'];
-			$token="c54127f2a601d197efffc968761bb711";
 			$check=array();
 			$uname=array();
 			$sql = "SELECT name,password FROM register_info WHERE email='{$email}'";
